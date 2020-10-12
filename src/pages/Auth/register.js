3fb80419430b937e834ecdad3d83081b0d0a10f8
Harrison <harrison.ekpobimi@sov.tech>
2020-10-12 23:21:19 +0200

    feat: new user registration

--- src/pages/Auth/register.js
+++ src/pages/Auth/register.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useContext, useState } from 'react';
+import { useMutation } from '@apollo/react-hooks';
+import { AuthContext } from '../../context/auth';
+import { useForm } from '../../utils/formHooks';
 import register from "../../assets/register.jpg";
+import { REGISTER_USER } from "../../graphql/mutations/register";
 
-const Register = () => {
+const Register = (props) => {
+  const context = useContext(AuthContext);
+  const [errors, setErrors] = useState({});
+
+  const { onChange, onSubmit, values } = useForm(registerUser, {
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  });
+
+  const [addUser, { loading }] = useMutation(REGISTER_USER, {
+    update(
+      _,
+      {
+        data: { register: userData }
+      }
+    ) {
+      context.login(userData);
+      props.history.push('/login');
+    },
+    onError(err) {
+      setErrors(err.graphQLErrors[0].extensions.exception.errors);
+    },
+    variables: values
+  });
+
+  function registerUser() {
+    addUser();
+  }
+
   return (
     <section className="cover-user bg-white">
       <div className="container-fluid px-0">
@@ -16,37 +50,8 @@ const Register = () => {
                   >
                     <div className="card-body p-0">
                       <h4 className="card-title text-center">Signup</h4>
-                      <form className="login-form mt-4">
+                      <form onSubmit={onSubmit} className="login-form mt-4">
                         <div className="row">
-                          <div className="col-md-6">
-                            <div className="form-group position-relative">
-                              <label>
-                                First name{" "}
-                                <span className="text-danger">*</span>
-                              </label>
-                              <input
-                                type="text"
-                                className="form-control"
-                                placeholder="First Name"
-                                name="s"
-                                required
-                              />
-                            </div>
-                          </div>
-                          <div className="col-md-6">
-                            <div className="form-group position-relative">
-                              <label>
-                                Last name <span className="text-danger">*</span>
-                              </label>
-                              <input
-                                type="text"
-                                className="form-control"
-                                placeholder="Last Name"
-                                name="s"
-                                required
-                              />
-                            </div>
-                          </div>
                           <div className="col-lg-12">
                             <div className="form-group position-relative">
                               <label>
@@ -58,7 +63,9 @@ const Register = () => {
                                 className="form-control"
                                 placeholder="Username"
                                 name="username"
-                                required
+                                value={values.username}
+                                error={errors.username ? true : false}
+                                onChange={onChange}
                               />
                             </div>
                           </div>
@@ -73,7 +80,9 @@ const Register = () => {
                                 className="form-control"
                                 placeholder="Email"
                                 name="email"
-                                required
+                                value={values.email}
+                                error={errors.email ? true : false}
+                                onChange={onChange}
                               />
                             </div>
                           </div>
@@ -86,7 +95,26 @@ const Register = () => {
                                 type="password"
                                 className="form-control"
                                 placeholder="Password"
-                                required
+                                name="password"
+                                value={values.password}
+                                error={errors.password ? true : false}
+                                onChange={onChange}
+                              />
+                            </div>
+                          </div>
+                          <div className="col-md-12">
+                            <div className="form-group position-relative">
+                              <label>
+                                Confirm Password <span className="text-danger">*</span>
+                              </label>
+                              <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Confirm Password"
+                                name="confirmPassword"
+                                value={values.confirmPassword}
+                                error={errors.confirmPassword ? true : false}
+                                onChange={onChange}
                               />
                             </div>
                           </div>
@@ -112,7 +140,8 @@ const Register = () => {
                           </div>
                           <div className="col-md-12">
                             <button className="btn btn-primary btn-block">
-                              Register
+                              Register {" "}
+                              {loading && (<span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>)}
                             </button>
                           </div>
                           <div className="mx-auto">
@@ -129,6 +158,18 @@ const Register = () => {
                             </p>
                           </div>
                         </div>
+                        <br />
+                        {Object.keys(errors).length > 0 && (
+                        <div className="pt-10">
+                          <ul>
+                            {Object.values(errors).map((value) => (
+                              <li style={{color: 'red'}} key={value}>
+                                {value}
+                              </li>
+                            ))}
+                          </ul>
+                        </div>
+                      )}
                       </form>
                     </div>
                   </div>
